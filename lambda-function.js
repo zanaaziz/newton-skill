afd0932323d7f25882f14f71fb56f22c8d712ecf
Zana Daniel <zanadaniel97@gmail.com>
2017-03-29 15:14:57 +0100

    Newton 1.0
    Able to solve basic differentiation and logarithmic problems.

---
+++ lambda-function.js
@@ -0,0 +1,107 @@
+var https = require('https')
+
+exports.handler = (event, context) => {
+
+  try {
+
+    if (event.session.new) {
+      // New Session
+      console.log("NEW SESSION")
+    }
+
+    switch (event.request.type) {
+
+      case "LaunchRequest":
+        // Launch Request
+        console.log(`LAUNCH REQUEST`)
+        context.succeed(
+          generateResponse(
+            buildSpeechletResponse("Mr. Newton says hello, he's ready to solve any calculus problems that you may have.", true),
+            {}
+          )
+        )
+        break;
+
+      case "IntentRequest":
+        // Intent Request
+        console.log(`INTENT REQUEST`)
+
+        switch(event.request.intent.name) {
+          case "FindDerivative":
+            var endpoint = `https://newton.now.sh/derive/${event.request.intent.slots.dBase.value}^${event.request.intent.slots.exponent.value}` // ENDPOINT GOES HERE
+            var body = ""
+            https.get(endpoint, (response) => {
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+                var data = JSON.parse(body)
+                var result = JSON.stringify(data.result);
+                context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(`The answer is ${result}`, true),
+                    {}
+                  )
+                )
+              })
+            })
+            break;
+
+            case "FindLogarithm":
+            var endpoint = `https://newton.now.sh/log/${event.request.intent.slots.log.value}|${event.request.intent.slots.lBase.value}` // ENDPOINT GOES HERE
+            var body = ""
+            https.get(endpoint, (response) => {
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+                var data = JSON.parse(body)
+                var result = JSON.stringify(data.result);
+                context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(`The answer is ${result}`, true),
+                    {}
+                  )
+                )
+              })
+            })
+            break;
+
+          default:
+            throw "Invalid intent"
+        }
+
+        break;
+
+      case "SessionEndedRequest":
+        // Session Ended Request
+        console.log(`SESSION ENDED REQUEST`)
+        break;
+
+      default:
+        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
+
+    }
+
+  } catch(error) { context.fail(`Exception: ${error}`) }
+
+}
+
+// Helpers
+buildSpeechletResponse = (outputText, shouldEndSession) => {
+
+  return {
+    outputSpeech: {
+      type: "PlainText",
+      text: outputText
+    },
+    shouldEndSession: shouldEndSession
+  }
+
+}
+
+generateResponse = (speechletResponse, sessionAttributes) => {
+
+  return {
+    version: "1.0",
+    sessionAttributes: sessionAttributes,
+    response: speechletResponse
+  }
+
+}
